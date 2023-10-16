76cbb3535d344e1a2fb26afff47bdd8f3dbf370e
kuntoajin <kuntoaji.engineer@gmail.com>
2023-10-16 17:03:54 +0700

    push

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -105,11 +105,12 @@ class _MyWidgetState extends State<TestWidget> {
             child: Column(
               children: [
                 Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
                       height: 90,
                       width: 140,
-                      margin: EdgeInsets.only(left: 20),
+                      margin: EdgeInsets.only(left: 20, bottom: 15),
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(15),
                           image: DecorationImage(
@@ -117,7 +118,135 @@ class _MyWidgetState extends State<TestWidget> {
                               image: NetworkImage(
                                   'https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2022/11/15071119/Gado-Gado-dan-Ketoprak-Mana-yang-Lebih-Rendah-Kalori.jpg'))),
                     ),
-                    Text('Ketoprak'),
+                    SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Ketoprak Pak Shin',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(height: 3),
+                        Text('Dengan sambal kacang',
+                            style: TextStyle(fontSize: 10, color: Colors.grey)),
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Text('Rp. 23.000',
+                                style: TextStyle(color: Colors.blueGrey)),
+                            SizedBox(width: 10),
+                            Row(
+                              children: [
+                                Icon(Icons.location_on,
+                                    color: AppColors.mainColor, size: 16),
+                                Text(
+                                  '4km',
+                                  style: TextStyle(fontSize: 12),
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 90,
+                      width: 140,
+                      margin: EdgeInsets.only(left: 20, bottom: 15),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  'https://cdn.idntimes.com/content-images/community/2021/08/cover-1d7e007c50ecd923926e64b9e704dee3-d4b12d15179c9cbeffe1033770043ca2_600x400.jpg'))),
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Bubur Koh Denis',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(height: 3),
+                        Text('Dengan sambal kacang',
+                            style: TextStyle(fontSize: 10, color: Colors.grey)),
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Text('Rp. 21.000',
+                                style: TextStyle(color: Colors.blueGrey)),
+                            SizedBox(width: 10),
+                            Row(
+                              children: [
+                                Icon(Icons.location_on,
+                                    color: AppColors.mainColor, size: 16),
+                                Text(
+                                  '2km',
+                                  style: TextStyle(fontSize: 12),
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 90,
+                      width: 140,
+                      margin: EdgeInsets.only(left: 20, bottom: 15),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  'https://asset-2.tstatic.net/belitung/foto/bank/images/20230613-Pecel-Lele-kesedapan-sejati-sejak-dalam-pandangan-mata.jpg'))),
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Pecel Lele Goreng',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(height: 3),
+                        Text('Paket pecel lele goreng sambal matah',
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontSize: 10, color: Colors.grey)),
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Text('Rp. 25.000',
+                                style: TextStyle(color: Colors.blueGrey)),
+                            SizedBox(width: 10),
+                            Row(
+                              children: [
+                                Icon(Icons.location_on,
+                                    color: AppColors.mainColor, size: 16),
+                                Text(
+                                  '2km',
+                                  style: TextStyle(fontSize: 12),
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    )
                   ],
                 )
               ],
@@ -168,7 +297,7 @@ class _MyWidgetState extends State<TestWidget> {
                 image: DecorationImage(
                     fit: BoxFit.cover,
                     image: NetworkImage(
-                        'https://awsimages.detik.net.id/community/media/visual/2023/10/06/presiden-jokowi-foto-biro-pers-sekretariat-presiden-4_169.jpeg'))),
+                        'https://media.sukabumiupdate.com/media/2023/01/20/1674217900_63ca89ac0f895_Ck15X6rBGVyRcdxXu9YC.jpg'))),
           ),
           Align(
               alignment: Alignment.bottomCenter,
@@ -192,7 +321,7 @@ class _MyWidgetState extends State<TestWidget> {
                     children: [
                       Container(
                         margin: EdgeInsets.only(top: 10),
-                        child: Text('Masakan cina',
+                        child: Text('Gorengan Bu Ima',
                             style: TextStyle(fontSize: 20, height: 0.5)),
                       ),
                       SizedBox(height: 10),
